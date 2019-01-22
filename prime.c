9cd7059a4a833d3244c5c0b5a5302dadf33acffb
root <natcastets@icloud.com>
2019-01-22 19:43:08 +0100

    pattern detection

--- prime.c
+++ prime.c
@@ -1,49 +1,69 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
- 
+
 MODULE_LICENSE("GPL");
 
+#define ENTRY_SYSCALL_64_RANGE 4032
+#define PATTERN_0_SIZE 9
+#define PATTERN_1_SIZE 6
+
 int init_module(void) {
-  int i;
-  /*
+  int i, j;
+  bool found;
   int do_syscall_64_offset;
   int sys_call_table_offset;
   unsigned char *do_syscall_64;
   unsigned long long *sys_call_table;
-  unsigned char *entry_SYSCALL_64 = (unsigned char*)(native_load_gs_index - 4032);
-  */
-  //unsigned long long *sys_call_table      = (unsigned long long *)0xffffffff81800200;
-  //unsigned long long *ia32_sys_call_table = (unsigned long long *)0xffffffff818015c0;
-  unsigned char *do_syscall_64              = (unsigned char *)0xffffffff81003af0;
-  unsigned char *entry_SYSCALL_64           = (unsigned char *)0xffffffff81618f10;
+  unsigned char *entry_SYSCALL_64 = (unsigned char*)(native_load_gs_index - ENTRY_SYSCALL_64_RANGE);
+  unsigned char pattern_0[PATTERN_0_SIZE] = {0x45, 0x31, 0xff, 0x48, 0x89, 0xc7, 0x48, 0x89, 0xe6};
+  unsigned char pattern_1[PATTERN_1_SIZE] = {0x48, 0x19, 0xc0, 0x48, 0x21, 0xc2};
   
   printk(KERN_INFO "prime module started\n");
 
-  printk(KERN_INFO "entry_SYSCALL_64\n");
-  for (i = 0; i < 4032; i += 8)
-    printk(KERN_INFO "%p : %02x %02x %02x %02x %02x %02x %02x %02x\n",
-	   entry_SYSCALL_64 + i,
-	   entry_SYSCALL_64[i],
-	   entry_SYSCALL_64[i + 1],
-	   entry_SYSCALL_64[i + 2],
-	   entry_SYSCALL_64[i + 3],
-	   entry_SYSCALL_64[i + 4],
-	   entry_SYSCALL_64[i + 5],
-	   entry_SYSCALL_64[i + 6],
-	   entry_SYSCALL_64[i + 7]);
+  for (i = 0; i < 1024; i++) {
+    found = true;
+    for (j = 0; i < PATTERN_0_SIZE; j++) {
+      if (entry_SYSCALL_64[i + j] != pattern_0[j]) {
+	found = false;
+	break;
+      }
+    }
 
-  printk(KERN_INFO "do_syscall_64\n");
-  for (i = 0; i < 1024; i += 8)
-    printk(KERN_INFO "%p : %02x %02x %02x %02x %02x %02x %02x %02x\n",
-	   do_syscall_64 + i,
-	   do_syscall_64[i],
-	   do_syscall_64[i + 1],
-	   do_syscall_64[i + 2],
-	   do_syscall_64[i + 3],
-	   do_syscall_64[i + 4],
-	   do_syscall_64[i + 5],
-	   do_syscall_64[i + 6],
-	   do_syscall_64[i + 7]);
+    if (found) {
+      do_syscall_64_offset = *(int *)(entry_SYSCALL_64 + i + 9 + 1);
+      do_syscall_64 = (unsigned char *)(entry_SYSCALL_64 + i + 9 + 1 + 4 + do_syscall_64_offset);
+      printk(KERN_INFO "call to do_syscall_64 at %p (%p)", entry_SYSCALL_64 + i + 9, do_syscall_64);
+      break;
+    } 
+  }
+
+  if (!do_syscall_64) {
+    printk(KERN_INFO "failed to find do_syscall_64 address\n");
+    return 1;
+  }
+
+  for (i = 0; i < 1024; i++) {
+    found = true;
+    for (j = 0; i < PATTERN_1_SIZE; j++) {
+      if (do_syscall_64[i + j] != pattern_1[j]) {
+	found = false;
+	break;
+      }
+    }
+
+    if (found) {
+      sys_call_table_offset = *(int *)(do_syscall_64 + i + 6 + 4);
+      sys_call_table = (unsigned long long *)sys_call_table_offset;
+      printk(KERN_INFO "call to sys_call_table at %p (%p)", do_syscall_64 + i + 6, sys_call_table); 
+      break;
+    } 
+  }
+
+  if (!do_syscall_64) {
+    printk(KERN_INFO "failed to find do_syscall_64 address\n");
+    return 1;
+  }
+
   
   /*
   for (i = 0; i < 4032; i++) {
@@ -55,7 +75,6 @@ int init_module(void) {
 	entry_SYSCALL_64[i + 5] == 0xe7) {
       do_syscall_64_offset = *(int *)(entry_SYSCALL_64 + i + 7);
       do_syscall_64 = (unsigned char *)(entry_SYSCALL_64 + i + 11 + do_syscall_64_offset);
-
       printk(KERN_INFO "call to do_syscall_64 at %p (%p)\n", entry_SYSCALL_64 + i + 6, do_syscall_64);
       break;
     }
@@ -63,7 +82,7 @@ int init_module(void) {
 
   if (!do_syscall_64) {
     printk(KERN_INFO "failed to find do_syscall_64 address\n");
-    return 0;
+    return 1;
   }
   
   for (i = 0; i < 1024; i++) {
@@ -83,10 +102,10 @@ int init_module(void) {
       break;
     }
   }
-
+  
   if (!sys_call_table) {
     printk(KERN_INFO "failed to find sys_call_table address\n");
-    return 0;
+    return 1;
   }
   */
   return 0;
