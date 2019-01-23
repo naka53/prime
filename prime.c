b164836612c2892c48f345c84b4f87afc5e2d775
nath <natcastets@icloud.com>
2019-01-23 13:36:25 +0100

    remove useless bool

--- prime.c
+++ prime.c
@@ -12,7 +12,6 @@ static void **sys_call_table;
 
 void search_sys_call_table(void) {
   int i, j;
-  bool pattern_found;
   int *do_syscall_64_offset;
   int *sys_call_table_offset;
   unsigned char *entry_SYSCALL_64 = (unsigned char *)(native_load_gs_index - ENTRY_SYSCALL_64_SIZE);
@@ -21,15 +20,11 @@ void search_sys_call_table(void) {
   unsigned char pattern_1[] = {0x48, 0x19, 0xc0, 0x48, 0x21, 0xc7};
 
   for (i = 0; i < SEARCH_RANGE; i++) {
-    pattern_found = true;
-    for (j = 0; j < PATTERN_0_SIZE; j++) {
-      if (entry_SYSCALL_64[i + j] != pattern_0[j]) {
-	pattern_found = false;
+    for (j = 0; j < PATTERN_0_SIZE; j++)
+      if (entry_SYSCALL_64[i + j] != pattern_0[j])
 	break;
-      }
-    }
 
-    if (pattern_found) {
+    if (j == PATTERN_0_SIZE) {
       do_syscall_64_offset = (int *)(entry_SYSCALL_64 + i + PATTERN_0_SIZE + 1);
       do_syscall_64 = (unsigned char *)(entry_SYSCALL_64 + i + PATTERN_0_SIZE + 1 + sizeof(int) + *do_syscall_64_offset);
       printk(KERN_INFO "call to do_syscall_64 at %p (%p)", entry_SYSCALL_64 + i + PATTERN_0_SIZE, do_syscall_64);
@@ -44,15 +39,11 @@ void search_sys_call_table(void) {
 
   
   for (i = 0; i < SEARCH_RANGE; i++) {
-    pattern_found = true;
-    for (j = 0; j < PATTERN_1_SIZE; j++) {
-      if (do_syscall_64[i + j] != pattern_1[j]) {
-	pattern_found = false;
+    for (j = 0; j < PATTERN_1_SIZE; j++)
+      if (do_syscall_64[i + j] != pattern_1[j])
 	break;
-      }
-    }
-
-    if (pattern_found) {
+    
+    if (j == PATTERN_1_SIZE) {
       sys_call_table_offset = (int *)(do_syscall_64 + i + PATTERN_1_SIZE + 4);
       sys_call_table = (void **)(*sys_call_table_offset);
       printk(KERN_INFO "call to sys_call_table at %p (%p)", do_syscall_64 + i + PATTERN_1_SIZE, sys_call_table); 
